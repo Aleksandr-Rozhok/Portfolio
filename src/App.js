b264bd0012dcdaa0c37eb3f5dd8b61ec272c27f8
Aleksandr <67546260+Aleksandr-Rozhok@users.noreply.github.com>
2021-02-05 13:01:16 +0200

    add: social network
    social network

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+import { BrowserRouter, Route } from "react-router-dom";
+
+
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route exact path='/dialogs' render={ () => <Dialogs dialogsPage={props.state.dialogsPage} dispatch={props.dispatch}/>} />
+          <Route path='/profile' render={ () => <Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
+          <Route path='/music' render={ () => <Music/>} />
+          <Route path='/news' render={ () => <News/>} />
+          <Route path='/settings' render={ () => <Settings/>} />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
