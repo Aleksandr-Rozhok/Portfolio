db17e1e393796fa1a312c1a08832569d68d20f15
Aleksandr <67546260+Aleksandr-Rozhok@users.noreply.github.com>
2021-02-05 13:11:16 +0200

    add: social network
    social network

---
+++ social network/src/components/Dialogs/DialogItems/DialogItems.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './DialogItems.module.css';
+import {NavLink} from 'react-router-dom';
+
+const DialogItems = (props) => {
+    return (
+      <div className={classes.activeLink + '' + classes.dialog}>
+          <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>     
+      </div>
+    );
+  }
+
+  
+
+  export default DialogItems;
